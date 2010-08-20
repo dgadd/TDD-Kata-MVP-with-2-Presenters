b107790f6b4d5be3f17bd129d882565beb4c4133
David Gadd <gaddzeit@yahoo.ca>
2010-08-20 09:18:48 -0700

    Added a WinForm implementation for both Presenters.

---
+++ App.Support/Repository/InMemoryTempDataRepository.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using App.Support.Domain;
+
+namespace App.Support.Repository
+{
+    public class InMemoryTempDataRepository : ITempDataRepository
+    {
+        public bool StoreMemberValues(Member member)
+        {
+            StoreMember.Member = member;
+            return true;
+        }
+
+        public Member GetMemberValues()
+        {
+            return StoreMember.Member;
+        }
+
+        public static class StoreMember
+        {
+            public static Member Member { get; set; }
+        }
+    }
+}
---
+++ WinFormMVP/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using App.Support.Domain;
+using App.Support.Presenter;
+using App.Support.Repository;
+using App.Support.View;
+
+namespace WinFormMVP
+{
+    public partial class Form1 : Form, IGatherMemberInfoView
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            new GatherMemberInfoPresenter(new InMemoryTempDataRepository(), this);
+        }
+
+        private void btnRenewGymMembership_Click(object sender, EventArgs e)
+        {
+            this.GatherMemberInfo(this, new GatherMemberInfoEventArgs
+            {
+                Member = new Member
+                {
+                    FirstName = txtFirstName.Text,
+                    LastName = txtLastName.Text,
+                    GymMembershipId = txtGymMembershipId.Text
+                }
+            });
+        }
+
+        public event EventHandler GatherMemberInfo;
+        public void GoToNextView()
+        {
+            var nextWindow = new ProcessingGymMembership();
+            nextWindow.Show();
+        }
+    }
+}
